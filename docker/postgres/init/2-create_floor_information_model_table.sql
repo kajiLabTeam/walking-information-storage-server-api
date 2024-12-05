6700bd386a013b5cb4247448aa1fb7fa66030162
karasuneo <yusei4520030401@gmail.com>
2024-12-05 17:18:37 +0900

    fix: coordinateテーブルの追加

--- docker/postgres/init/2-create_floor_information_model_table.sql
+++ docker/postgres/init/2-create_floor_information_model_table.sql
@@ -8,13 +8,30 @@ CREATE TABLE floor_maps (
     PRIMARY KEY (id, floor_information_id)
 );
 
+CREATE TABLE coordinates (
+    id VARCHAR(26),
+    x DECIMAL NOT NULL,
+    y DECIMAL NOT NULL,
+    is_walkable BOOLEAN NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    floor_id VARCHAR(26) REFERENCES floors(id),
+    PRIMARY KEY (id)
+);
 
-CREATE TABLE fp_models (
+CREATE TABLE geomagnetic_fingerprinting (
+    id VARCHAR(26),
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    coordinate_id VARCHAR(26) REFERENCES coordinates(id),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE wifi_fingerprinting (
     id VARCHAR(26),
     mac_address VARCHAR(17),
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    floor_information_id VARCHAR(26) UNIQUE REFERENCES floor_information(id),
-    PRIMARY KEY (id, floor_information_id)
+    coordinate_id VARCHAR(26) REFERENCES coordinates(id),
+    PRIMARY KEY (id)
 );
-
