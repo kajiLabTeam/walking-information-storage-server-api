6eeba06c0e57e9ce917faed45b6cf10b33524577
karasuneo <yusei4520030401@gmail.com>
2024-12-07 15:37:07 +0900

    add: ヘルスチェック用のハンドラを追加

--- server/main.go
+++ server/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/kajiLabTeam/walking-information-storage-server-api/server/presentation/controllers"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// 環境変数の読み込み
+	err := godotenv.Load("../.env")
+	if err != nil {
+		panic(err)
+	}
+
+	r := gin.Default()
+
+	// 以下にコントローラーを追加していく
+	controllers.HealthCheckHandler(r)
+
+	r.Run(":8000")
 }
