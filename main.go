aefefd7016586b8322f1d1fce85469bcfb2f1fe6
karasuneo <yusei4520030401@gmail.com>
2024-12-08 01:18:26 +0000

    fix:

--- main.go
+++ main.go
@@ -1,26 +1,22 @@
 package main
 
 import (
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
-	"github.com/kajiLabTeam/walking-information-storage-server-api/app/infrastructure"
+	"github.com/kajiLabTeam/walking-information-storage-server-api/app/presentation/controllers"
 )
 
 func main() {
 	// 環境変数の読み込み
-	err := godotenv.Load("../.env")
+	err := godotenv.Load(".env")
 	if err != nil {
 		panic(err)
 	}
 
-	_, err = infrastructure.Connect()
-	if err != nil {
-		panic(err)
-	}
+	r := gin.Default()
 
-	// r := gin.Default()
+	// 以下にコントローラーを追加
+	controllers.HealthCheckHandler(r)
 
-	// // 以下にコントローラーを追加
-	// controllers.HealthCheckHandler(r)
-
-	// r.Run(":8080")
+	r.Run(":8080")
 }
