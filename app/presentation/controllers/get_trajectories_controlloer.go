056d6df7687fdb386bf0e4d52e0c92a2164d195b
MihoMiho14134 <153794136+MihoMiho14134@users.noreply.github.com>
2024-12-10 04:39:40 +0000

    add:EstimationTrajectoriesId add:EstimationPosition

--- app/presentation/controllers/get_trajectories_controlloer.go
+++ app/presentation/controllers/get_trajectories_controlloer.go
@@ -4,14 +4,23 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// 構造体の変数名は、パスカルケース(大文字)
+
+// EstimationTrajectoriesId(軌跡)は配列
+//EstimationPosition(推定座標)は２重配列
+
 type GetTrajectoriesResponse struct {
-	Status string `json:"status"`
+	FloorMapImage            string `json:"floor_map_image"`
+	EstimationTrajectoriesId int    `json:"estimation_trajectories_id"`
+	EstimationPosition       int    `json:"id"`
 }
 
 func GetTrajectoriesController(r *gin.Engine) {
 	r.GET("/api", func(c *gin.Context) {
 		res := GetTrajectoriesResponse{
-			Status: "OK",
+			FloorMapImage:            "gsgsgfs12144",
+			EstimationTrajectoriesId: 1,
+			EstimationPosition:       1,
 		}
 
 		c.JSON(200, res)
