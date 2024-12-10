a3ab3fcff2c3507f42b9fc53d24a7686a4535098
MihoMiho14134 <153794136+MihoMiho14134@users.noreply.github.com>
2024-12-10 09:46:17 +0000

    app:"estimation_trajectories_id"&"estimation_position"

--- app/presentation/controllers/get_trajectories_controlloer.go
+++ app/presentation/controllers/get_trajectories_controlloer.go
@@ -6,23 +6,35 @@ import (
 
 // 構造体の変数名は、パスカルケース(大文字)
 
-// EstimationTrajectoriesId(軌跡)は配列
+//EstimationTrajectoriesId(推定軌跡)は配列
 //EstimationPosition(推定座標)は２重配列
 
 type GetTrajectoriesResponse struct {
-	FloorMapImage            string `json:"floor_map_image"`
-	EstimationTrajectoriesId int    `json:"estimation_trajectories_id"`
-	EstimationPosition       int    `json:"id"`
+	FloorMapImage string `json:"floor_map_image"`
+	EstimationTrajectories
+}
+type EstimationTrajectories struct {
+	EstimationTrajectoriesID int `json:"estimation_trajectories_id"`
+	EstimationPosition
+}
+type EstimationPosition struct {
+	ID int `json:"id"`
+	X  int `json:"x"`
+	Y  int `json:"y"`
 }
 
 func GetTrajectoriesController(r *gin.Engine) {
 	r.GET("/api", func(c *gin.Context) {
 		res := GetTrajectoriesResponse{
-			FloorMapImage:            "gsgsgfs12144",
-			EstimationTrajectoriesId: 1,
-			EstimationPosition:       1,
+			FloorMapImage: "gsgsgfs12144",
+			EstimationTrajectories: EstimationTrajectories{
+				EstimationTrajectoriesID: 123,
+				EstimationPosition: EstimationPosition{
+					ID: 1, X: 3, Y: 2,
+				},
+			},
 		}
-
 		c.JSON(200, res)
+
 	})
 }
